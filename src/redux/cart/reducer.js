6ae806cf7490ffd481161d7a898916dc51040156
albanovs <albvnovs@gmail.com>
2023-04-17 00:47:07 +0600

    localStorage

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,19 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const items = localStorage.getItem('cartItems') !== null ? JSON.parse(localStorage.getItem('cartItems')) : []
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
-        itemsInCart: []
+        itemsInCart: items
     },
     reducers: {
         setItemInCart: (state, action) => {
             state.itemsInCart.push(action.payload)
+            localStorage.setItem('cartItems', JSON.stringify(state.itemsInCart.map((item) => item)))
         },
         deleteItemFromCart: (state, action) => {
             state.itemsInCart = state.itemsInCart.filter(film => film.id !== action.payload)
+            localStorage.setItem('cartItems', JSON.stringify(state.itemsInCart.map((item) => item)))
         }
     }
 })
 
-export const {setItemInCart, deleteItemFromCart } = cartSlice.actions
+export const { setItemInCart, deleteItemFromCart } = cartSlice.actions
 export default cartSlice.reducer
