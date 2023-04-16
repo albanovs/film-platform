583937eac92e7ce89ec2f91cb3b7ce3f81c7b3bf
albanovs <albvnovs@gmail.com>
2023-04-16 19:49:25 +0600

    res

--- src/components/film-item/film-item.jsx
+++ src/components/film-item/film-item.jsx
@@ -18,15 +18,17 @@ export const FilmItem = ({ film }) => {
     }
 
     return (
-        <div className='film-item' onClick={handleClick}>
-            <FilmCover image={film.image} />
-            <div className='details'>
-                <span className='film-title'>{film.title}</span>
-                <div className='film-genre'>
-                    {film.genres.map((genre) => <FilmGenre genre={genre} key={genre} />)}
-                </div>
-                <div className='filmItem-buy'>
-                    <FilmBuy film={film} />
+        <div className='responsive-item'>
+            <div className='film-item' onClick={handleClick}>
+                <FilmCover image={film.image} />
+                <div className='details'>
+                    <span className='film-title'>{film.title}</span>
+                    <div className='film-genre'>
+                        {film.genres.map((genre) => <FilmGenre genre={genre} key={genre} />)}
+                    </div>
+                    <div className='filmItem-buy'>
+                        <FilmBuy film={film} />
+                    </div>
                 </div>
             </div>
         </div>
