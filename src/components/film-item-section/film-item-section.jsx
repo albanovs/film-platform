79ac27128ded76351a129464c084ecffcf0f5db7
albanovs <albvnovs@gmail.com>
2023-04-20 19:37:32 +0600

    desktop vers Section

---
+++ src/components/film-item-section/film-item-section.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { FilmBuy } from '../film-buy'
+import './film-item-section.css'
+import { setCurrentFilm } from '../../redux/films/reducer'
+
+export const FilmItemSection = ({ film }) => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const handleClick = () => {
+        dispatch(setCurrentFilm(film))
+        navigate(`/film/${film.title}`)
+    }
+    return (
+        <div className='film-section'>
+            <div onClick={handleClick}><img width={200} height={300} src={film.image_smoll} alt='cover'/></div>
+            <div className='film-section__details'>
+                <div><span className='section-title'>Название: </span> {film.title}</div>
+                <div><span className='section-title'>год: </span> {film.year}</div>
+                <div><span className='section-title'>Производство: </span> {film.country}</div>
+                <div><span className='section-title'>Жанр: </span> {film.genres}</div>
+                <div><span className='section-title__des'>Описание: </span> {film.description}</div>
+            </div>
+            <div className='film-section__buy'>
+                <FilmBuy film={film} />
+            </div>
+        </div>
+    )
+}
--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -3,15 +3,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './home-page.css'
 import { FilmItem } from "../../components/film-item/film-item";
-
+import { FilmItemSection } from "../../components/film-item-section/film-item-section";
 
 
 export const HomePage = ({ data }) => {
     return (
         <div>
-            <h1 className="global-title">Топ фильмы</h1>
+            {/* <h1 className="global-margin">Топ фильмы</h1> */}
             <div className="home-page">
                 {data.map(film => <FilmItem film={film} key={film.id} />)}
+            </div>
+            <div className="section-one global-margin">
+                <h1 className="news">Новинки </h1>
+                <div className="section-one__items">
+                    {data.map(film => <FilmItemSection film={film} key={film.id} />)}
+                </div>
             </div>
         </div>
     )
