d3f0fb0b3337fe37833070a3e70d12b7098ee965
albanovs <albvnovs@gmail.com>
2023-04-20 17:53:26 +0600

    news

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { FilmItem } from '../../components/film-item'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './home-page.css'
+import { FilmItem } from "../../components/film-item/film-item";
 
 
 
 export const HomePage = ({ data }) => {
     return (
-        <div className="home-page">
-            {data.map(film => <FilmItem film={film} key={film.id} />)}
+        <div>
+            <h1 className="global-title">Топ фильмы</h1>
+            <div className="home-page">
+                {data.map(film => <FilmItem film={film} key={film.id} />)}
+            </div>
         </div>
     )
 }
